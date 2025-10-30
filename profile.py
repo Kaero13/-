69267247ded299412a3dd56348758a332cd12854
Kaero <v19163420283@gmail.com>
2025-10-30 08:36:09 +0300

    Добавил запись профилей в json
    дозаписывание пока не реализовано

--- profile.py
+++ profile.py
@@ -16,4 +16,18 @@ def fix_path(path)
         return path
 
 def add_and_create_profile_to_json(Nik, Password, path, json_path, json_name = 'Profile_Data.json')
-    pass
+    full_output_path = os.path.dirname(json_path, json_name)
+
+    parent_dir = os.path.dirname(full_output_path)
+    if parent_dir:
+        os.makedirs(parent_dir, exist_ok=True)
+
+    profile_dict = {}
+    profile_dict[Nik] = {"Password" : Password, "Video_path" : path}
+
+    if not os.path.exists(json_path):
+        with open(full_output_path, 'w', encoding="utf-8") as f:
+            json.dump(profile_dict, f, ensure_ascii=False, indent=2)
+
+    else:
+        pass
