55bc4414286d11e71cdc4f23189069ba8d2def5b
Kaero <v19163420283@gmail.com>
2025-11-04 19:25:51 +0300

    Изменил profile_window
    Добавлено сообщение о том, что нет json файла, нет профиля или неправильный пароль/ник.

--- video redactor/video redactor/profile_seletc_window.py
+++ video redactor/video redactor/profile_seletc_window.py
@@ -28,31 +28,60 @@ def select_window():
     Nick_lable.grid(row = 0, column = 1,sticky=tk.W)
     Nick_pol = tk.Entry(input_frame, width=30, textvariable=Nick)
     Nick_pol.grid(row=0, column=2, padx=5, pady=5)
+
     pasword_lable = tk.Label(input_frame, text="Пароль:")
     pasword_lable.grid(row=1, column=1,sticky=tk.W)
     pasword_pol = tk.Entry(input_frame, width=30, textvariable=Password)
     pasword_pol.grid(row=1, column=2, padx=5, pady=5)
+
     folder_path_lable = tk.Label(input_frame, text="Путь к папке пользователя:")
-    folder_path_lable.grid(row=2, column=1)
+    folder_path_lable.grid(row=2, column=1,sticky=tk.W)
     folder_pol = tk.Entry(input_frame, width=30, textvariable=Folder)
     folder_pol.grid(row=2, column=2, padx=5, pady=5)
 
+    messages_lable_pole = tk.Label(input_frame, text="")
+    messages_lable_pole.grid(row=3, column=1, padx=5, pady=5)
+
+    def message(Bool):
+        if Bool == True:
+            return "Профиль успешно добавлен"
+        else:
+            return "Заполните поля ник, пароль, путь к папке профиля."
+
+    def sing_message(Bool):
+        if Bool == True:
+            return "Файл профиля не существует.\n Для начала работы\n создайте новый профиль."
+        else:
+            return "Файл профиля не существует\n или не правильный пароль/ник.\n Для начала работы\n создайте новый профиль."
 
     def registr():
-        add_and_create_profile_to_json(Nick.get(), Password.get(), folder_pol.get(), json_path)
+        global messages_lable_pole
+        a = add_and_create_profile_to_json(Nick.get(), Password.get(), folder_pol.get(), json_path)
+
+        if 'messages_lable_pole' in globals():
+            messages_lable_pole.destroy()
+
+        messages_lable_pole = tk.Label(input_frame, text=message(a))
+        messages_lable_pole.grid(row=3, column = 1, padx = 5, pady = 5)
 
     def window_sing():
         registr_button.grid_forget()
         folder_pol.grid_forget()
-        sing_window_button.grid_forget()
         folder_path_lable.grid_forget()
+        sing_window_button.grid_forget()
+
         sing_button = tk.Button(root, text="Войти", command=sing)
         sing_button.grid(row=1, column=1, padx=5, pady=5)
 
+        messages_lable_pole_sing = tk.Label(input_frame, text="")
+        messages_lable_pole_sing.grid(row=2, column=1, padx=5, pady=5)
+
     def sing():
+        global messages_lable_pole_sing
         path = None
         a = Nick.get()
         b = Password.get()
+        c = True
         try:
             with open(r"C:\Users\Acer\PycharmProjects\PythonProject\video redactor\Profile_data\Profile_Data.json", "r", encoding="utf-8") as f:
                 data = json.load(f)
@@ -61,12 +90,26 @@ def select_window():
                     path = data[a]["Video_path"]
                     root.selected_path = path
                     root.destroy()
+            elif a == "" or b == "":
+                c = False
+                if 'messages_lable_pole_sing' in globals():
+                    messages_lable_pole_sing.destroy()
+
+                messages_lable_pole_sing = tk.Label(input_frame, text=sing_message(c))
+                messages_lable_pole_sing.grid(row=3, column=1, padx=5, pady=5)
+
         except:
-            print("Файл профиля не существует. Для начала работы создайте новый профиль")
+            if 'messages_lable_pole_sing' in globals():
+                messages_lable_pole_sing.destroy()
+
+            messages_lable_pole_sing = tk.Label(input_frame, text=sing_message(c))
+            messages_lable_pole_sing.grid(row=3, column=1, padx=5, pady=5)
+
+
     registr_button = tk.Button(root, text="Зарегистрировать", command=registr)
-    registr_button.grid(row=3, column=1)
+    registr_button.grid(row=3, column=2)
     sing_window_button = tk.Button(root, text="Окно входа",  command=window_sing)
-    sing_window_button.grid(row=3, column=2)
+    sing_window_button.grid(row=3, column=3)
 
     root.mainloop()
     return root.selected_path
